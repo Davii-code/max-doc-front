ae9d577b71a4848567c9a4de96f44b9b6130f694
davif <davifariagoncalves@gmail.com>
2025-01-29 17:35:50 -0300

    first commit

---
+++ src/app/component/docs-list/docs-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-docs-list',
+  imports: [],
+  templateUrl: './docs-list.component.html',
+  styleUrl: './docs-list.component.css'
+})
+export class DocsListComponent {
+
+}
